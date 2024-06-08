881c65a4b4c253b2d13a3dfc0cfd914958e93854
RonghaiHe <hrhkjys@qq.com>
2024-06-08 15:23:20 +0800

    fix little bug in shell

--- deployment/uwb/run.sh
+++ deployment/uwb/run.sh
@@ -2,7 +2,8 @@
 
 # 运行例子：$ bash run.sh 0 
 
-id=$1
+# 尝试使用第一个参数作为id，如果未提供，则使用默认值0
+id=${1:0}
 
 source devel/setup.bash
-roslaunch linktrack _multiple_tags.launch id:=${id}
+roslaunch linktrack_multiple_tags.launch id:=${id}
