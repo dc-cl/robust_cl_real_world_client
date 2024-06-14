eafc310ba9c74e96ac35ee513f455eb90ff7a5db
Harry <2323471022@qq.com>
2024-06-14 17:30:08 +0800

    test in pycharm

--- scripts/util/utils_real.py
+++ scripts/util/utils_real.py
@@ -351,7 +351,7 @@ def Measurement():
             if mea_count < numbers:
                 mea_count += 1
                 for m in mea:
-                    mea_rela_all[mea_count, id, 0] = m  # 将m赋值给mea_rela_all数组中对应的位置
+                    mea_rela_all[mea_count, id, :] = m  # 将m赋值给mea_rela_all数组中对应的位置
 # --------------------
 
 
