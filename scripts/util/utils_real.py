3eab349265990914658e87ef79ea4c18f0aa7940
Law Harry <75736304+LHarry-coder@users.noreply.github.com>
2024-06-17 13:19:52 +0800

    Update utils_real.py

--- scripts/util/utils_real.py
+++ scripts/util/utils_real.py
@@ -89,8 +89,7 @@ class TopicSubscriber:
 
     def callback(self, msg_data):
         dis_data = msg_data.nodes.dis
-        self.dis_list = [1]
-        # self.dis_list.append(dis_data)  # 存储 node 的 dis 数据
+        self.dis_list.append(dis_data)  # 存储 node 的 dis 数据
         # self.id = msg_data.id
         # 遍历 nodes 数组并获取 dis 数据  LinktrackNode2[] nodes
         # for node in msg_data.nodes:
