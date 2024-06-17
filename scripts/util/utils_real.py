3cf55e6d3917e939a4c8fe3e8274e42e4b0eb96b
Harry <2323471022@qq.com>
2024-06-17 21:02:55 +0800

    test in pycharm

--- scripts/util/utils_real.py
+++ scripts/util/utils_real.py
@@ -88,8 +88,9 @@ class TopicSubscriber:
         self.dis_list = []
 
     def callback(self, msg_data):
-        dis_data = msg_data.nodes[2]
-        self.dis_list.append(dis_data)  # 存储 node 的 dis 数据
+        nodes = msg_data.nodes
+        for node in nodes:
+            self.dislist.append(node.dis)  # 存储 node 的 dis 数据
         # self.id = msg_data.id
         # 遍历 nodes 数组并获取 dis 数据  LinktrackNode2[] nodes
         # for node in msg_data.nodes:
