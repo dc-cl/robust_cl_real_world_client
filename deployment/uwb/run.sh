c4ce617e15c137c0f7a7f311afa392e6463d890c
wp2 <wp2@sysu.com>
2024-07-12 16:03:22 +0800

    Modify little in run.sh

--- deployment/uwb/run.sh
+++ deployment/uwb/run.sh
@@ -3,7 +3,10 @@
 # 运行例子：$ bash run.sh 0 
 
 # 尝试使用第一个参数作为id，如果未提供，则使用默认值0
-id=${1:0}
+id=${1}
+if [ -z "$id" ]; then
+    num=0
+fi
 
 source devel/setup.bash
 roslaunch robust_cl_real_world_client linktrack_GT.launch id:=${id}
